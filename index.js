45628035f767ef2e4a8027264292b691d8ec05f4
Yogi-123 <myogi.jp@gmail.com>
2020-08-18 09:46:44 +0700

    mengisi tugas

---
+++ index.js
@@ -0,0 +1,49 @@
+// buat conditional if
+
+let divisi = "HR"
+
+if (divisi == "HR") {
+    console.log("Bertugas melakukan recruitment calon pegawai baru")
+} else if (difisi == "Web Developer") {
+    console.log("Bertugas merancang dan membuat web")
+} else {
+    console.log("Divisi tidak dikenali")
+}
+
+// conditional untuk membandingkan dua nilai
+
+let nilaiA = 10
+let nilaiB = 20
+
+if (nilaiA > nilaiB) {
+    console.log("nilai A lebih besar dari nilai B")
+} else if (nilaiA < nilaiB) {
+    console.log("nilai A lebih kecil dari nilai B")
+} else {
+    console.log("nilai A sama dengan nilai B")
+}
+
+// switch case mengetahui nama hari
+switch (namaHari) {
+    case 1:
+        console.log("senin");
+        break;
+    case 2:
+        console.log("selasa");
+        break;
+    case 3:
+        console.log("rabu");
+        break;
+    case 4:
+        console.log("kamis");
+        break;
+    case 5:
+        console.log("jumat");
+        break;
+    case 6:
+        console.log("sabtu");
+    case 7:
+        console.log("minggu");
+}
+
+// switch case game sederhana
